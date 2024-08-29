1ebb4f5e658aaaa43384249e3994091ba72a265a
Trevor <treramey@gmail.com>
2024-08-28 21:49:20 -0500

    WIP

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -1,4 +1,36 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextDocsMDX from "fumadocs-mdx/config";
 
-export default nextConfig;
+const withMDX = createNextDocsMDX();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@aws-sdk/s3-request-presigner"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default withMDX(nextConfig);
---
+++ apps/dashboard/src/middleware.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = { matcher: ["/dashboard/(.*)"] };
+
+export async function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
